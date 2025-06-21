0bcebd4dddb099163314d1a9d808520816a6f306
Prasad Magdum <magdumprasad194@gmail.com>
2025-06-21 13:52:12 +0530

    updated files

--- pathnova_backend/pathnova_backend/settings.py
+++ pathnova_backend/pathnova_backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,9 +101,9 @@ WSGI_APPLICATION = 'pathnova_backend.wsgi.application'
 
 from mongoengine import connect
 connect(
-    db='pathnova',
-    host='localhost',
-    port=27017
+    db=os.getenv('db'),
+    host=os.getenv('host'),
+    port=os.getenv('port')
 )
 
 
